1b6e0a079ea3fb99a7ad7bde9d61a69cf4b9dfa4
Dongmin Kim <dongkim2@c2r17s2.42gyeongsan.kr>
2026-04-09 10:08:37 +0900

    얼추 할 일 정리해둠

--- main.c
+++ main.c
@@ -11,66 +11,50 @@
 
 int	process_make(int argc, char **argv)
 {
-	char	**cmd;
     pid_t   pid;
 	int		fd[2];
 
-	//file = "/bin/free";
-	//printf("pid: %d\n", pid);
-	//pid = getpid();
 	if (pipe(fd) == -1)
-	{
-		printf("ㅈ됨 ㅅㄱ\n");
-	}
-	//printf("1: %d   2: %d\n", fd[0], fd[1]);
-	cmd = malloc(sizeof(char *) * 4);
+		return (-1);
 	pid = fork();
-	//첫번째 이후로는 로직 반대로 사용하면 될듯!
-    if (pid == 0)
+	if (pid == (pid_t)-1)
+		return (-1);
+    if (pid == (pid_t)0)
     {
 		dup2(fd[1], 1);
-		//printf("자식: 1: %d   2: %d\n", fd[0], fd[1]);
 		close(fd[0]);
-		cmd[0] = "/bin/cat";
-		cmd[1] = "test.txt";
-		cmd[2] = NULL;
-		printf("자식");
-		execve(cmd[0], cmd, NULL);
-    }
-    else
-    {
-		int status;
-		dup2(fd[0], 0);
 		close(fd[1]);
-		cmd[0] = "/bin/wc";
-		cmd[1] = "-l";
-		cmd[2] = NULL;
-		printf("부모");
-		execve(cmd[0], cmd, NULL);
+		execve(argv[0], argv, NULL);
     }
+	dup2(fd[0], 0);
+	close(fd[0]);
+	close(fd[1]);
+	//execve(argv[0], argv, NULL);
+	return (0);
 }
 
 int	main(int argc, char **argv)
 {
 	char	**file;
 	char	*sh;
+	int		i;
+	int		argc_index;
 
-	file = malloc(sizeof(char *) * 4);
+	file = malloc(sizeof(char *) * 5);
 	sh = malloc(sizeof(char *) * 10);
-    file[3] = NULL;
-	// printf("%s\n", argv[0]);
-	// printf("%s\n", argv[1]);
-	// printf("%s\n", argv[2]);
-	int i = 0;
-	while (++i < argc)
+	i = 0;
+	argc_index = 0;
+	while (argc_index < argc - 3)
 	{
-		strcpy(sh, argv[i]);
-		file[0] = sh;
-		file[1] = NULL;
-		printf("%s\n", file[0]);
+		file[0] = argv[++argc_index];
+		file[1] = argv[++argc_index];
+		file[2] = NULL;
 		process_make(2, file);
-		i;
 	}
+	file[0] = argv[++argc_index];
+	file[1] = argv[++argc_index];
+	file[2] = NULL;
+	execve(file[0], file, NULL);
 	
 	return (0);
 }
