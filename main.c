b1ab4f5208b4f34fae43cd89e642825f8ee117e5
Dongmin Kim <dongkim2@c2r17s2.42gyeongsan.kr>
2026-04-09 10:20:20 +0900

    생각 못한 부분 추가...

--- main.c
+++ main.c
@@ -44,6 +44,11 @@ int	main(int argc, char **argv)
 	sh = malloc(sizeof(char *) * 10);
 	i = 0;
 	argc_index = 0;
+	printf("%s\n", argv[0]);
+	printf("%s\n", argv[1]);
+	printf("%s\n", argv[2]);
+	printf("%s\n", argv[3]);
+	printf("%s\n", argv[4]);
 	while (argc_index < argc - 3)
 	{
 		file[0] = argv[++argc_index];
